68197441e10111888e4ef3a9e63e205ce68bcdf8
Pedro Oliveira <opedramos.oliveira@gmail.com>
2023-12-09 14:50:51 -0300

    Adiciona novas configs

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-e87"
+    bucket         = "terraform-state-3pp"
     key            = "eks-cluster/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
